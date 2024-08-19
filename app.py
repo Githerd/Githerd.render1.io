eb330036b56ada9a977fca35c883772658112d78
Githerd <158222320+Githerd@users.noreply.github.com>
2024-08-19 10:33:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -20,16 +20,16 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587  
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
+app.config['MAIL_USERNAME'] = os.getenv('kmat.adebisi@gmail.com')
+app.config['MAIL_PASSWORD'] = os.getenv('yumyumsugar_1')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('Kareemat Adebisi')
 
 # Database Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Secret key for session management
-app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key')
+app.secret_key = os.getenv('SECRET_KEY', 'yumyumsugar_1')
 
 # Initialize extensions
 db = SQLAlchemy(app)
@@ -152,7 +152,7 @@ def contact():
         
         msg = Message('New Contact Form Submission',
                       sender=app.config['MAIL_DEFAULT_SENDER'],
-                      recipients=['your-email@example.com'])  # Replace with your email
+                      recipients=['kmat.adebisi@gmail.com]) 
         msg.body = f'''
         Name: {name}
         Email: {email}
